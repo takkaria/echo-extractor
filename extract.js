5742ba9c93a577f7071a1852376c7db1d3ff1ab6
Andi Sidwell <andi@takkaria.org>
2015-02-28 17:53:08 +0000

    Functional extractor

--- extract.js
+++ extract.js
@@ -1,50 +1,80 @@
+// build using the crunchinator http://ted.mielczarek.org/code/mozilla/bookmarklet.html
+// max length 2048
+
 d = document;
 d.qs = d.querySelector;
 d.qsa = d.querySelectorAll;
-d.qst = function querySelectorText(match) {
-	var e = d.qs(match);
-	if (e) return e.innerText;
+
+// Modified from jquery
+getText = function (elem) {
+	var node,
+		ret = '',
+		nodeType = elem.nodeType;
+
+	if ( nodeType === 1 || nodeType === 9 || nodeType === 11 ) {
+		if ( typeof elem.textContent === 'string' ) {
+			return elem.textContent;
+		} else {
+			// Traverse its children
+			for ( elem = elem.firstChild; elem; elem = elem.nextSibling ) {
+				ret += getText( elem );
+			}
+		}
+	} else if ( nodeType === 3 || nodeType === 4 ) {
+		return elem.nodeValue;
+	}
+	// Do not include comment or processing instruction nodes
+
+	return ret;
 }
 
-function makeUrl(base, params) {
-	var url = base + "?";
+qst = function(match) {
+	var e = d.qs(match);
+	if (e) return getText(e);
+}
+
+makeUrl = function(base, params) {
+	var url = base + '?';
 	for (var prop in params)
-		url += prop + "=" + encodeURIComponent(params[prop]) + "&";
+		url += prop + '=' + encodeURIComponent(params[prop]) + '&';
 	return url;
 }
 
-function provideEventData(data) {
-	d.location.href = makeUrl("https://echomanchester.net/event/add", data);
+eventData = function(data) {
+	d.location.href = makeUrl('https://echomanchester.net/event/add', data);
 }
 
-function facebook() {
+if (d.location.host == "www.facebook.com") {
 	// See more
-	var c = d.qs(".see_more_link");
+	var c = d.qs('.see_more_link');
 	if (c)
 		c.click();
 
-	var dt = new Date(d.qs("[itemprop=startDate]").getAttribute("content"));
+	var dt = new Date(d.qs('[itemprop=startDate]').getAttribute('content'));
 
-	// Make the actual URL.
-	provideEventData({
-		title: d.qst("#event_header_info a"),
-		location: d.qsa("._5xhk")[1].innerText + ", " + d.qsa("._5xhp")[1].innerText,
-		blurb: d.qst("#event_description"),
+	eventData({
+		title: qst('#event_header_info a'),
+		location: d.qsa('._5xhk')[1].textContent + ', ' + d.qsa('._5xhp')[1].textContent,
+		blurb: qst('#event_description'),
 		url: d.location.href,
-		date1: dt.getFullYear() + "-" + (dt.getMonth() + 1) + "-" + dt.getDate(),
-		time1: dt.getHours() + ":" + dt.getMinutes(),
+		date1: dt.getFullYear() + '-' + (dt.getMonth() + 1) + '-' + dt.getDate(),
+		time1: dt.getHours() + ':' + dt.getMinutes(),
 	});
-}
+} else if (d.location.host == "www.eventbrite.com") {
+	var dt1 = d.qs('.dtstart .value-title').title;
+	var dt2 = d.qs('.dtend .value-title').title;
 
-function eventbrite() {
-	// Make the actual URL.
-	provideEventData({
-		title: d.qst("#event_header h1 span.summary"),
-		hosted: d.qst("#event_header h2"),
-		location: d.qst(".location.vcard"),
-		blurb: d.qst(".description"),
+	eventData({
+		title: qst('#event_header h1 span.summary'),
+		hosted: qst('#event_header h2').trim(),
+		location: qst('.location.vcard').trim(),
+		blurb: qst('.description').trim(),
 		url: d.location.href,
-		startdt: d.qs(".dtstart .value-title").title,
-		enddt: d.qs(".dtend .value-title").title,
+		date1: dt1.substr(0, 10),
+		time1: dt1.substr(11, 5),
+		date2: dt2.substr(0, 10),
+		time2: dt2.substr(11, 5),
 	});
+} else {
+	alert("Can't handle this webpage.");
 }
